10adf66db98a38eee0918854d4b44b6b8e65bf6e
Yosuke Adachi <doruji1982@gmail.com>
2026-01-11 22:52:03 +0900

    tessoku:基数変換

--- src/tessoku-book/A04_BinaryRepresentation1.js
+++ src/tessoku-book/A04_BinaryRepresentation1.js
@@ -3,18 +3,21 @@ const input = fs.readFileSync(0, 'utf8').trim();
 const lines = input.split('\n');
 const [N] = lines[0].split(' ').map(Number);
 
-// const arr = [512, 256, 128, 64, 32, 16, 8, 4, 2, 1];
-// const answer = new Uint8Array(10);
-
-// // n
-// for (let i = 0; i < arr.length; i++) {
-//     answer[i] = (N / arr[i]) % 2;
-// }
-
-// console.log(answer.join(''));
-
+// 10 > 2
 let s = '';
+// for (let i = 9; i >= 0; i--) {
+//     s += (N >> i) & 1;
+// }
 for (let i = 9; i >= 0; i--) {
-    s += (N >> i) & 1;
+    s += Math.floor((N / (2 ** i))) % 2;
 }
+// console.log(s);
+
+// 2 > 10
+let sum = 0;
+for (let i = 9; i >= 0; i--) {
+    sum += s[s.length - 1 - i] * (2 ** i);
+    // console.log(`s[s.length - 1 - i] = ${s[s.length - 1 - i]}, 2 ** i = ${(2 ** i)}, sum = ${sum.toString()}`);
+}
+
 console.log(s);
